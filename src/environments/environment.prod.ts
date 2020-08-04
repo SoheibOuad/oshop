b5e23afaba43f5f26982bdd014127dbba49e7f2e
sol <souadfel@3c-e.com>
2020-08-04 13:50:56 +0200

    add new project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAT4575Dtgo5lN1jC2UZ_ppRG2INY1Ysm0',
+    authDomain: 'oshop-8feb6.firebaseapp.com',
+    databaseURL: 'https://oshop-8feb6.firebaseio.com',
+    projectId: 'oshop-8feb6',
+    storageBucket: 'oshop-8feb6.appspot.com',
+    messagingSenderId: '335846524049',
+    appId: '1:335846524049:web:9aeeb8f1a9552514d22b30',
+    measurementId: 'G-YREB9T46DS'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAT4575Dtgo5lN1jC2UZ_ppRG2INY1Ysm0',
+    authDomain: 'oshop-8feb6.firebaseapp.com',
+    databaseURL: 'https://oshop-8feb6.firebaseio.com',
+    projectId: 'oshop-8feb6',
+    storageBucket: 'oshop-8feb6.appspot.com',
+    messagingSenderId: '335846524049',
+    appId: '1:335846524049:web:9aeeb8f1a9552514d22b30',
+    measurementId: 'G-YREB9T46DS'
+  }
 };
 
 /*
