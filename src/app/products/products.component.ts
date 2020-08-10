34596c1f3f63b4dca794fb449021429b7595493b
sol <souadfel@3c-e.com>
2020-08-10 09:53:14 +0200

    implementation of product catalog

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ProductService} from '../product.service';
+import {Product} from '../models/product';
+import {Subscription} from 'rxjs';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnDestroy {
+  products: Product[]= [];
+  filtredProducts: Product[] = [];
+  myPicturesRef;
+  categories;
+  subscription: Subscription;
+  category: string;
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService , private db: AngularFireDatabase ) {
+    this.myPicturesRef = this.db.list('/products', ref => ref
+      .orderByChild('name'));
+    this.subscription=this.myPicturesRef
+      .snapshotChanges().subscribe((res) => {
+        this.products = res.map(change => ({key: change.payload.key, ...change.payload.val()}));
 
-  constructor() { }
+        //Get procuts with filters
+        route.queryParamMap.subscribe(params => {
+          this.category=params.get('category');
+
+          this.filtredProducts = (this.category) ?
+            this.products.filter(p =>p.category === this.category) :
+            this.products;
+        });
+      });
+
+    // Get all the categoris.
+    this.myPicturesRef = this.db.list('/categories', ref => ref
+      .orderByChild('name'));
+    this.myPicturesRef
+      .snapshotChanges().subscribe((res) => {
+      this.categories = res.map(change => ({key: change.payload.key, ...change.payload.val()}));
+    });
+
+
+  }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
+
 
 }
